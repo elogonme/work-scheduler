96b1b8249ebbcad66a08c2c0d09b9f9484911063
Eldar <elogon@gmail.com>
2020-10-30 11:56:46 -0400

    Create pseudo code for main algorithm

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Start script after page is loaded - all js code goes in here
+$(document).ready(function() {
+
+    var dateToday = moment().format();
+
+    console.log(dateToday);
+
+// Get todays date
+// Set todays date in p #currentDay
+
+// display on the page day work hours list 
+// add on each hour textarea for input and button to save
+// load day schedule from localStorage
+// fill in day hours with date about schedule from loaded data.
+// Each hour is color coded with past, present and future hours (grey, red, green)
+// Add event listner for save buttons
+// Depending wich save buttonis clicked get textarea and save under corresponding hour into loacl storage
+// Refresh saved day work schedule
+
+// Additonal?
+// Clear each hour?
+// Clear all day?
+// Clear all stored data from local storage
+
+
+
+
+});
